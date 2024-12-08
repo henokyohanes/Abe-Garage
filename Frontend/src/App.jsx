f82b64e759775f54d2a97f875e353dceaf0f92cf
henokyohanesdev <henokyohanes8@gmail.com>
2024-12-07 23:13:56 -0600

    header component fully responsive styling implemented.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./Contexts/AuthContext";
 import AppRoutes from "./routes/routes";
+import "@fontsource/inter";
 import "./App.css";
+import Header from "./Components/Header/Header";
 
 const App = () => (
-  <AuthProvider>
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
-  </AuthProvider>
+    <AuthProvider>
+      <BrowserRouter>
+        {/* <AppRoutes /> */}
+  <Header />
+
+      </BrowserRouter>
+    </AuthProvider>
 );
 
 export default App;
--- Frontend/src/Components/Header/Header.jsx
+++ Frontend/src/Components/Header/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../Contexts/AuthContext";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 import loginService from "../../services/login.service";
 import logo from "../../assets/images/logo.png";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Style from "./Header.module.css";
+import { FaBars } from "react-icons/fa";
 
 const Header = (props) => {
 
@@ -19,33 +22,109 @@ const Header = (props) => {
     };
 
     return (
+        // <header className={Style.header}>
+        //     <div className={Style.topBar}>
+        //         <p>
+        //             <span className={Style.tagline}>
+        //                 Enjoy the Beso while we fix your car
+        //             </span>
+        //             <span className={Style.workingHours}>
+        //                 Monday - Saturday | 7:00AM - 6:00PM
+        //             </span>
+        //         </p>
+        //         <div>
+        //             {isLogged ? (
+        //                 <p className={Style.contactInfo}>Welcome: <strong>{employee?.employee_first_name}</strong></p>
+        //             ) : (
+        //                 <p className={Style.contactInfo}>Call Abe: <strong>1800 456 7890</strong></p>
+        //             )}
+
+        //         </div>
+
+        //     </div>
+        //     <div className={Style.mainHeader}>
+        //         <div className={Style.logo}>
+        //             <img src={logo} alt="ABE Garage Logo" />
+        //         </div>
+        //         <NavDropdown
+        //             title="Browse"
+        //             id="navbar-dropdown"
+        //             className="custom-toggle"
+        //         >
+        //             <NavDropdown.Item>Home</NavDropdown.Item>
+        //             <NavDropdown.Item>About Us</NavDropdown.Item>
+        //             <NavDropdown.Item>Services</NavDropdown.Item>
+        //             <NavDropdown.Item>Contact Us</NavDropdown.Item>
+        //         </NavDropdown>
+        //         <nav className={Style.navMenu}>
+        //             <ul>
+        //                 <li>
+        //                     <a href="/">Home</a>
+        //                 </li>
+        //                 <li>
+        //                     <a href="/about-us">About Us</a>
+        //                 </li>
+        //                 <li>
+        //                     <a href="/services">Services</a>
+        //                 </li>
+        //                 <li>
+        //                     <a href="/contact-us">Contact Us</a>
+        //                 </li>
+        //             </ul>
+        //             <div>
+        //                 {isLogged ? (
+        //                     <Link to="/" className={Style.signInButton} onClick={logOut} >Log out</Link>
+        //                 ) : (
+        //                     <Link to="/login" className={Style.signInButton}>Sign In</Link>
+        //                 )}
+
+        //             </div>
+
+        //         </nav>
+        //     </div>
+        // </header>
+
+
+
+
+
+
+
+
         <header className={Style.header}>
             <div className={Style.topBar}>
-                <p>
-                    <span className={Style.tagline}>
+                <div className={Style.leftTopBar}>
+                    <div className={`${Style.tagline} d-none d-md-block`}>
                         Enjoy the Beso while we fix your car
-                    </span>
-                    <span className={Style.workingHours}>
-                        Monday - Saturday 7:00AM - 6:00PM
-                    </span>
-                </p>
-                <div className="right-column">
+                    </div>
+                    <div className={Style.workingHours}>
+                        Monday - Saturday | 7:00AM - 6:00PM
+                    </div>
+                </div>
+                <div>
                     {isLogged ? (
-                        <div className="link-btn">
-                            <div className="phone-number"><strong>Welcome {employee?.employee_first_name}</strong></div>
-                        </div>
+                        <div className={Style.contactInfo}>Welcome: <strong>{employee?.employee_first_name}</strong></div>
                     ) : (
-                        <p className={Style.contactInfo}>Call Abe:<span className={Style.phone}> 1800 456 7890</span></p>
+                        <div className={Style.contactInfo}>Call Abe: <strong>1800 456 7890</strong></div>
                     )}
+
                 </div>
 
             </div>
             <div className={Style.mainHeader}>
-                <div className={Style.logo}>
+                <NavDropdown
+                    title={<FaBars size={30}/>}
+                    id="navbar-dropdown"
+                    className={`${Style.browseButton} d-md-none`}
+                    >
+                    <NavDropdown.Item>Home</NavDropdown.Item>
+                    <NavDropdown.Item>About Us</NavDropdown.Item>
+                    <NavDropdown.Item>Services</NavDropdown.Item>
+                    <NavDropdown.Item>Contact Us</NavDropdown.Item>
+                </NavDropdown>
                     <img src={logo} alt="ABE Garage Logo" />
-                </div>
-                <nav className={Style.navMenu}>
-                    <ul>
+                <dir className={Style.navMenu}>
+                    <ul className="d-none d-md-flex">
                         <li>
                             <a href="/">Home</a>
                         </li>
@@ -59,13 +138,16 @@ const Header = (props) => {
                             <a href="/contact-us">Contact Us</a>
                         </li>
                     </ul>
-                    <div>{isLogged ? (
-                        <Link to="/" className={Style.signInButton} onClick={logOut} >Log out</Link>
-                    ) : (
-                        <Link to="/login" className={Style.signInButton}>Sign In</Link>
-                    )}</div>
+                    <div className={Style.signInButton}>
+                        {isLogged ? (
+                            <Link to="/" className={Style.signInButto} onClick={logOut} >Log out</Link>
+                        ) : (
+                            <Link to="/login" className={Style.signInButto}>Sign In</Link>
+                        )}
 
-                </nav>
+                    </div>
+
+                </dir>
             </div>
         </header>
     );
