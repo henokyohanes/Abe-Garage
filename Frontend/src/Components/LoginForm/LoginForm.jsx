43260f0ae659870e49ac5334fa324ec88cd50ab2
henokyohanesdev <henokyohanes8@gmail.com>
2024-12-14 15:43:30 -0600

    login page authentication functionality implemented.

--- Frontend/src/Components/LoginForm/LoginForm.jsx
+++ Frontend/src/Components/LoginForm/LoginForm.jsx
@@ -4,7 +4,79 @@ import loginService from "../../services/login.service";
 import Styles from "./LoginForm.module.css";
 
 const LoginForm = () => {
-  
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [employee_email, setEmail] = useState("");
+  const [employee_password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [serverError, setServerError] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Handle client side validations here
+    let valid = true;
+
+    // Email validation
+    if (!employee_email) {
+      setEmailError("Please enter your email address first");
+      valid = false;
+    } else if (!employee_email.includes("@")) {
+      setEmailError("Invalid email format");
+    } else {
+      const regex = /^\S+@\S+\.\S+$/;
+      if (!regex.test(employee_email)) {
+        setEmailError("Invalid email format");
+        valid = false;
+      } else {
+        setEmailError("");
+      }
+    }
+    if (!employee_password || employee_password.length < 6) {
+      setPasswordError("Password must be at least 6 characters long");
+      valid = false;
+    } else {
+      setPasswordError("");
+    }
+    if (!valid) {
+      return;
+    }
+
+    const formData = {
+      employee_email,
+      employee_password,
+    };
+
+    // Call the service
+    const loginEmployee = loginService.logIn(formData);
+    loginEmployee
+      .then((response) => {
+        if (response.status === "success") {
+          // Save the user in the local storage
+          if (response.data.employee_token) {
+            localStorage.setItem("employee", JSON.stringify(response.data));
+          }
+
+          // Redirect the user to the dashboard
+          navigate("/admin");
+          if (location.pathname === "/login") {
+            navigate("/admin");
+            window.location.replace("/");
+          } else {
+            window.location.reload();
+          }
+        } else {
+          setServerError(response.message);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        setServerError("An error has occurred. Please try again later.");
+      });
+  };
+
   return (
     <section className={Styles.contactSection}>
       <div className={Styles.container}>
