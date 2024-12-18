b67ecd8f8e65ea9df8573650e4f201c1e8d20d58
henokyohanesdev <henokyohanes8@gmail.com>
2024-12-18 11:49:57 -0600

    employees page fully responsive styling implemented.

--- Frontend/src/Pages/Admin/Employees/Employees.jsx
+++ Frontend/src/Pages/Admin/Employees/Employees.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import employeeService from "../../../services/employees.service";
-import StyleS from "./Employees.module.css";
+import styles from "./Employees.module.css";
+import AdminMenu from "../../../Components/AdminMenu/AdminMenu";
+import Layout from "../../../Layout/Layout";
 
 const EmployeeList = () => {
     const [employees, setEmployees] = useState([]);
@@ -40,7 +43,6 @@ const EmployeeList = () => {
         }
     };
 
-
     const handleEdit = (id) => {
         navigate(`/edit-employee/${id}`);
     };
@@ -49,40 +51,62 @@ const EmployeeList = () => {
     if (error) return <p>Error: {error}</p>;
 
     return (
-        <div className={StyleS.employeeList}>
-            <h2>Employees</h2>
-            <table className={StyleS.table}>
-                <thead>
-                    <tr>
-                        <th>Active</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Email</th>
-                        <th>Phone</th>
-                        <th>Added Date</th>
-                        <th>Role</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {employees.map((employee) => (
-                        <tr key={employee.id}>
-                            <td>{employee.active ? "Yes" : "No"}</td>
-                            <td>{employee.firstName}</td>
-                            <td>{employee.lastName}</td>
-                            <td>{employee.email}</td>
-                            <td>{employee.phone}</td>
-                            <td>{employee.addedDate}</td>
-                            <td>{employee.role}</td>
-                            <td>
-                                <button onClick={() => handleEdit(employee.id)}><FaEdit /></button>
-                                <button onClick={() => handleDelete(employee.id)}><MdDelete /></button>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+        <Layout>
+            <div className={`${styles.container} row g-0`}>
+                <div className={`${styles.adminMenu} d-none d-lg-block col-2`}>
+                    <AdminMenu />
+                </div>
+                <div className={`${styles.adminMenuContainer} d-block d-lg-none`}>
+                    <div className={styles.adminMenuTitle}>
+                        <h2>Admin Menu</h2>
+                    </div>
+                    <div className={styles.listGroup}>
+                        <Link to="/admin/dashboard" className={styles.listGroupItem}>Dashboard</Link>
+                        <Link to="/admin/orders" className={styles.listGroupItem}>Orders</Link>
+                        <Link to="/admin/order" className={styles.listGroupItem}>New order</Link>
+                        <Link to="/admin/add-employee" className={styles.listGroupItem}>Add employee</Link>
+                        <Link to="/admin/employees" className={styles.listGroupItem}>Employees</Link>
+                        <Link to="/admin/add-customer" className={styles.listGroupItem}>Add customer</Link>
+                        <Link to="/admin/customers" className={styles.listGroupItem}>Customers</Link>
+                        <Link to="/admin/services" className={styles.listGroupItem}>Services</Link>
+                    </div>
+                </div>
+                <div className={`${styles.employeeList} col-12 col-lg-10`}>
+                    <h2>Employees <span>____</span></h2>
+                    <table className={styles.table}>
+                        <thead>
+                            <tr>
+                                <th>Active</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Email</th>
+                                <th>Phone</th>
+                                <th>Added Date</th>
+                                <th>Role</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {employees.map((employee) => (
+                                <tr key={employee.id}>
+                                    <td>{employee.active ? "Yes" : "No"}</td>
+                                    <td>{employee.firstName}</td>
+                                    <td>{employee.lastName}</td>
+                                    <td>{employee.email}</td>
+                                    <td>{employee.phone}</td>
+                                    <td>{employee.addedDate}</td>
+                                    <td>{employee.role}</td>
+                                    <td>
+                                        <button onClick={() => handleEdit(employee.id)}><FaEdit /></button>
+                                        <button onClick={() => handleDelete(employee.id)}><MdDelete /></button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </Layout>
     );
 };
 
