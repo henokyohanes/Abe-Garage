8142b6da5e12a91e559814b99924882ad3d70a1b
henokyohanesdev <henokyohanes8@gmail.com>
2025-05-06 00:29:30 -0500

    some adjustment done on banner page  and styling.

--- Frontend/src/Components/Banner/Banner.jsx
+++ Frontend/src/Components/Banner/Banner.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaChevronRight } from 'react-icons/fa';
 import style from "./Banner.module.css"
 
 const Banner = () => {
@@ -9,11 +10,12 @@ const Banner = () => {
       <p>Working since 2000 <span>______</span></p>
       <h1>Tuneup Your Car to Next Level</h1>
       <div className={style.bannerButtons}>
-        <Link to="https://www.youtube.com/watch?v=CZ3U5oHLyl8" target="_blank">
+        <a href="https://www.youtube.com/watch?v=CZ3U5oHLyl8" target="_blank">
           <i className="fab fa-youtube" />
-        </Link>
+        </a>
         <p>Watch Intro Video<br />about us</p>
       </div>
+      <Link className={style.appointment}>Make an Appointment <FaChevronRight /></Link>
     </div>
   );
 }
