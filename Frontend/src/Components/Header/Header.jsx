f012b447bb93bd558f7fc1f3172698990adab1a8
henokyohanesdev <henokyohanes8@gmail.com>
2025-05-07 00:10:16 -0500

    profile image functionality implemented on header component.

--- Frontend/src/Components/Header/Header.jsx
+++ Frontend/src/Components/Header/Header.jsx
@@ -2,11 +2,35 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../Contexts/AuthContext";
 import { NavDropdown } from "react-bootstrap";
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaChevronDown, FaUser, FaSignOutAlt } from "react-icons/fa";
+import { RiAccountCircleFill } from "react-icons/ri";
 import loginService from "../../services/login.service";
 import logo from "../../assets/images/logo.png";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Style from "./Header.module.css";
+
+// Profile Image component
+// const ProfileImage = ({ user }) => {
+//     return (
+//         <div className={Style.profileImgWrapper}>
+//             <div className={Style.profileImgContainer}>
+//                 {user.profileimg ? (
+//                     <img
+//                         src={`${axiosImageURL}${user.profileimg}`}
+//                         className={Style.profileImg}
+//                         alt="Profile Image"
+//                         loading="lazy"
+//                     />
+//                 ) : (
+//                     <RiAccountCircleFill className={Style.profileImgCircle} />
+//                 )}
+//             </div>
+//             <div className={Style.profileArrow}>
+//                 <FontAwesomeIcon icon={faChevronDown} className={Style.arrowIcon} />
+//             </div>
+//         </div>
+//     );
+// };
 
 const Header = () => {
 
@@ -52,29 +76,60 @@ const Header = () => {
             <div className={Style.mainHeader}>
                 <NavDropdown title={<FaBars size={35} />} className="d-md-none">
                     <NavDropdown.Item as={Link} to="/">Home</NavDropdown.Item>
-                    <NavDropdown.Item as={Link} to="/about-us">About Us</NavDropdown.Item>
-                    <NavDropdown.Item as={Link} to="/abe-services">Services</NavDropdown.Item>
-                    <NavDropdown.Item as={Link} to="/contact-us">Contact Us</NavDropdown.Item>
                     {isLogged && (
                         <NavDropdown.Item as={Link} to="/dashboard">{getRoleText()}</NavDropdown.Item>
                     )}
+                    <NavDropdown.Item as={Link} to="/about-us">About Us</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/abe-services">Services</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/contact-us">Contact Us</NavDropdown.Item>
                 </NavDropdown>
-                <Link to="/"><img src={logo} alt="ABE Garage Logo" loading="lazy"/></Link>
+                <Link to="/"><img src={logo} alt="ABE Garage Logo" loading="lazy" /></Link>
                 <div className={Style.navMenu}>
                     <ul className="d-none d-md-flex">
                         <li><Link to="/">Home</Link></li>
-                        <li><Link to="/about-us">About Us</Link></li>
-                        <li><Link to="/abe-services">Services</Link></li>
-                        <li><Link to="/contact-us">Contact Us</Link></li>
                         {isLogged && (
                             <li><Link to="/dashboard">{getRoleText()}</Link></li>
                         )}
+                        <li><Link to="/about-us">About Us</Link></li>
+                        <li><Link to="/abe-services">Services</Link></li>
+                        <li><Link to="/contact-us">Contact Us</Link></li>
                     </ul>
                     <div>
                         {isLogged ? (
-                            <Link to="/" className={Style.signInButton} onClick={logOut}>Log out</Link>
+                            <NavDropdown
+                                title={<div className={Style.profileImgWrapper}>
+                                    <div className={Style.profileImgContainer}>
+                                        {employee.profileimg ? (
+                                            <img
+                                                src={`${axiosImageURL}${user.profileimg}`}
+                                                className={Style.profileImg}
+                                                alt="Profile Image"
+                                                loading="lazy"
+                                            />
+                                        ) : (
+                                            <RiAccountCircleFill className={Style.profileImgCircle} />
+                                        )}
+                                    </div>
+                                    <div className={Style.profileArrow}>
+                                        <FaChevronDown className={Style.arrowIcon} />
+                                    </div>
+                                </div>}
+                            >
+                                <NavDropdown.Item as={Link} to="/Account">
+                                    <span className={Style.icon}>
+                                        <FaUser />
+                                    </span>
+                                    Account Settings
+                                </NavDropdown.Item>
+                                <NavDropdown.Item onClick={logOut}>
+                                    <span className={Style.icon}>
+                                        <FaSignOutAlt />
+                                    </span>
+                                    Logout
+                                </NavDropdown.Item>
+                            </NavDropdown>
                         ) : (
-                            <Link to="/login" className={Style.signInButton}>Sign In</Link>
+                            <Link to="/Auth" className={Style.signInButton}>Register | Sign In</Link>
                         )}
                     </div>
                 </div>
