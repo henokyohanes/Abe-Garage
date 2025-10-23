e816957b135cac04b3777c0dd89c4e8f1644dad6
henokyohanesdev <henokyohanes8@gmail.com>
2025-10-22 22:37:28 -0500

    my order page component and styling updated.

--- Frontend/src/Pages/MyOrders/MyOrders.jsx
+++ Frontend/src/Pages/MyOrders/MyOrders.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate, useLocation } from "react-router-dom";
 import { useAuth } from "../../Contexts/AuthContext";
-import { FaEdit } from "react-icons/fa";
+// import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import Swal from "sweetalert2";
 import Layout from "../../Layout/Layout";
@@ -17,7 +17,7 @@ import styles from "./MyOrders.module.css";
 const MyOrders = () => {
 
     const { isLogged, setIsLogged, user, isAdmin, isManager, isEmployee } = useAuth();
-    
+
     const id = user?.customer_id;
     const [orders, setOrders] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -26,6 +26,8 @@ const MyOrders = () => {
     const location = useLocation();
     const params = new URLSearchParams(location.search);
     const redirectUrl = params.get("redirect");
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 2;
 
     // Fetch customer data, vehicles, and orders when id changes
     useEffect(() => {
@@ -42,6 +44,7 @@ const MyOrders = () => {
         try {
             const orderData = await orderService.fetchCustomerOrders(id);
             setOrders(orderData.data);
+            console.log("orderData", orderData.data);
         } catch (error) {
             console.error("Error fetching orders:", error);
             setError(true);
@@ -51,71 +54,87 @@ const MyOrders = () => {
     };
 
     // Delete an order of the customer
-    const handleDeleteOrder = async (id) => {
+    // const handleDeleteOrder = async (id) => {
 
-        try {
-            const result = await Swal.fire({
-            title: "Are you sure you want to delete this order?",
-            html: "This action cannot be undone.",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonText: "Yes !",
-            customClass: {
-                popup: styles.popup,
-                confirmButton: styles.confirmButton,
-                cancelButton: styles.cancelButton,
-                icon: styles.icon,
-                title: styles.warningTitle,
-                htmlContainer: styles.text,
-            },
-        });
-            if (!result.isConfirmed) return;
-                setLoading(true);
-                setError(false);
-                    await orderService.deleteOrder(id);
-                    setOrders(orders.filter((order) => order.order_id !== id));
+    //     try {
+    //         const result = await Swal.fire({
+    //         title: "Are you sure you want to delete this order?",
+    //         html: "This action cannot be undone.",
+    //         icon: "warning",
+    //         showCancelButton: true,
+    //         confirmButtonText: "Yes !",
+    //         customClass: {
+    //             popup: styles.popup,
+    //             confirmButton: styles.confirmButton,
+    //             cancelButton: styles.cancelButton,
+    //             icon: styles.icon,
+    //             title: styles.warningTitle,
+    //             htmlContainer: styles.text,
+    //         },
+    //     });
+    //         if (!result.isConfirmed) return;
+    //             setLoading(true);
+    //             setError(false);
+    //                 await orderService.deleteOrder(id);
+    //                 setOrders(orders.filter((order) => order.order_id !== id));
 
-                    await Swal.fire({
-                        title: "Deleted!",
-                        html: "The order has been deleted.",
-                        icon: "success",
-                        customClass: {
-                            popup: styles.popup,
-                            confirmButton: styles.confirmButton,
-                            icon: styles.icon,
-                            title: styles.successTitle,
-                            htmlContainer: styles.text,
-                        },
-                    });
-                } catch (error) {
-                    console.error("Error deleting order:", error);
-                    if (error === "Failed") {
-                        setError(true);
-                    } else {             
-                        Swal.fire({
-                            title: "Error!",
-                            html: "Failed to delete order. Please try again!",
-                            icon: "error",
-                            customClass: {
-                                popup: styles.popup,
-                                confirmButton: styles.confirmButton,
-                                icon: styles.icon,
-                                title: styles.errorTitle,
-                                htmlContainer: styles.text,
-                            },
-                        });
-                    }
-                } finally {
-                    setLoading(false);
-                }
-    };
+    //                 await Swal.fire({
+    //                     title: "Deleted!",
+    //                     html: "The order has been deleted.",
+    //                     icon: "success",
+    //                     customClass: {
+    //                         popup: styles.popup,
+    //                         confirmButton: styles.confirmButton,
+    //                         icon: styles.icon,
+    //                         title: styles.successTitle,
+    //                         htmlContainer: styles.text,
+    //                     },
+    //                 });
+    //             } catch (error) {
+    //                 console.error("Error deleting order:", error);
+    //                 if (error === "Failed") {
+    //                     setError(true);
+    //                 } else {             
+    //                     Swal.fire({
+    //                         title: "Error!",
+    //                         html: "Failed to delete order. Please try again!",
+    //                         icon: "error",
+    //                         customClass: {
+    //                             popup: styles.popup,
+    //                             confirmButton: styles.confirmButton,
+    //                             icon: styles.icon,
+    //                             title: styles.errorTitle,
+    //                             htmlContainer: styles.text,
+    //                         },
+    //                     });
+    //                 }
+    //             } finally {
+    //                 setLoading(false);
+    //             }
+    // };
 
     // Get the order status
-    function getOrderStatus(status) {
-        if (status === 0) return "Received";
-        if (status === 1) return "In Progress";
-        if (status === 2) return "Completed";
+    function getOrderStatus(order, pickup) {
+        if (order === 0 && pickup === 0) return "Received";
+        if (order >= 1 && pickup === 0) return "In Progress";
+        if (order === 2 && pickup >= 1) return "Completed";
         return "Pending";
+    };
+
+    // Calculate total pages
+    const totalPages = Math.ceil(orders.length / itemsPerPage);
+    const displayedOrders = orders.slice(
+        (currentPage - 1) * itemsPerPage,
+        currentPage * itemsPerPage
+    );
+
+    // Function to handle page change
+    const handlePageChange = (direction) => {
+        if (direction === "next" && currentPage < totalPages) {
+            setCurrentPage(currentPage + 1);
+        } else if (direction === "prev" && currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
     };
 
     return (
@@ -125,40 +144,55 @@ const MyOrders = () => {
                 <div className="d-block d-md-none"><AdminMenuMobile /></div>
                 <div className="col-12 col-md-9">
                     {!loading && !error ? (
-                            <div className={styles.container}>
-                                <div className={styles.allInfo}>
-                                    <h2>Orders of {user?.customer_first_name}</h2>
-                                    <div className={styles.orderInfo}>
-                                        {orders && orders.length > 0 ? (
-                                            orders.map((order, index) => (
-                                                <div key={index}>
-                                                    <div className={styles.deleteIcon} onClick={() => handleDeleteOrder(order.order_id)}>
+                        <div className={styles.container}>
+                            <div className={styles.allInfo}>
+                                <h2>Orders of {user?.customer_first_name} <span>_____</span></h2>
+                                <div className={styles.orderInfo}>
+                                    {orders && orders.length > 0 ? (
+                                        displayedOrders.map((order, index) => (
+                                            <div key={index}>
+                                                {/* <div className={styles.deleteIcon} onClick={() => handleDeleteOrder(order.order_id)}>
                                                         <MdDelete />
-                                                    </div>
-                                                    <div className={styles.orderCard}>
+                                                    </div> */}
+                                                <div className={styles.orderCard} onClick={() => navigate(`/order-details/${order.order_id}`)}>
+                                                    <div>
                                                         <p><strong>Order #: </strong>{order.order_id}</p>
                                                         <p>
                                                             <strong>Vehicle: </strong>
                                                             {order.vehicle_year} {order.vehicle_make} {order.vehicle_model}
                                                         </p>
                                                         <p><strong>Date: </strong>{order.order_date.split("T")[0]}</p>
-                                                        <p><strong>Status: </strong>{getOrderStatus(order.order_status)}</p>
+                                                        <p><strong>Status: </strong>{getOrderStatus(order.order_status, order.pickup_status)}</p>
                                                         <p><strong>Total: </strong>${order.order_total_price}</p>
-                                                        <p>
-                                                            <strong>Edit Order Info:</strong>
-                                                            <span onClick={() => navigate(`/edit-order/${order.order_id}`)}>
-                                                                <FaEdit />
-                                                            </span>
-                                                        </p>
                                                     </div>
+                                                    <button>➡</button>
                                                 </div>
-                                            ))
-                                        ) : (
-                                            <p className={styles.noMessage}>No orders found</p>
-                                        )}
-                                    </div>
+                                            </div>
+                                        ))
+                                    ) : (
+                                        <p className={styles.noMessage}>No orders found</p>
+                                    )}
                                 </div>
                             </div>
+
+                            {/* Pagination */}
+                            {orders.length > 0 && <div className={styles.pagination}>
+                                <button
+                                    onClick={() => handlePageChange("prev")}
+                                    disabled={currentPage === 1}
+                                >
+                                    Previous
+                                </button>
+                                <span>Page {currentPage} of {totalPages}</span>
+                                <button
+                                    onClick={() => handlePageChange("next")}
+                                    disabled={currentPage === totalPages}
+                                >
+                                    Next
+                                </button>
+                            </div>}
+
+                        </div>
                     ) : error ? <NotFound /> : <Loader />}
                 </div>
             </div>
